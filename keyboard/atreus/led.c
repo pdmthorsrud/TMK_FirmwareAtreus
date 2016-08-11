914701a9f69ed70e2b313a2532229852dc63b110
pdthorsr <sagittarius@live.no>
2016-08-11 23:20:20 +0200

    Dunno man

--- keyboard/atreus/led.c
+++ keyboard/atreus/led.c
@@ -24,11 +24,11 @@ void led_set(uint8_t usb_led)
 {
     if (usb_led & (1<<USB_LED_CAPS_LOCK)) {
         // output low
-        DDRD |= (1<<5);
-        PORTD &= ~(1<<5);
+        DDRD |= (1<<4);
+        PORTD &= ~(1<<4);
     } else {
         // Hi-Z
-        DDRD &= ~(1<<5);
-        PORTD &= ~(1<<5);
+        DDRD &= ~(1<<4);
+        PORTD &= ~(1<<4);
     }
 }
